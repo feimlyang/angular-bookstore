44deff80d923285907adf64b000f61a0bfd2264d
feimlyang <manlin1119@gmail.com>
2020-12-06 08:21:53 -0500

    lab

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {AuthenticationService} from './authentication/authentication.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  getLogin(): string {
+    return this.authService.isLoggedIn() ? 'Logout' : 'Login';
+  }
+  constructor(private authService: AuthenticationService) {}
+}
